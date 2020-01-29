ece210356a66b533935d80286c8a077aa32d149c
RylanFerrer <rylanferrer@gmail.com>
2020-01-29 14:00:28 -0800

    refactored header

--- client/src/Components/Home/Header.js
+++ client/src/Components/Home/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-export default function Header() {
+export default function Header(props) {
+    const {title} = props
     return (
             <div className = "home__header">
                 <div className = "home__header-nav">
                 <i className="fas home__icon fa-2x fa-bars"></i>
                 </div>
                 <div className = "home__header-container">
-                    <h3 className = "home__header-heading">My Accounts</h3>
+                    <h3 className = "home__header-heading">{title}</h3>
                 </div>
             </div>
     )
