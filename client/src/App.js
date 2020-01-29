1943e7001efa940670c2a9cf3c9e29e6b639c3b9
RylanFerrer <rylanferrer@gmail.com>
2020-01-29 13:09:17 -0800

    added home component to wrap around all other ones

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import './Styles/main.css'
-import "./Components/Home/HomeHeader"
-import "./Components/Home/HomeBanner"
-import HomeHeader from './Components/Home/HomeHeader';
-import HomeBanner from './Components/Home/HomeBanner';
-import HomeSignup from "./Components/Home/HomeSignup"
+import Home from './Components/Home/Home'
 function App() {
   return (
     <div >
-      <HomeHeader/>
-      <HomeBanner/>
-      <HomeSignup/>
+      <Home/>
     </div>
   );
 }
--- client/src/Components/Home/HomeBanner.js
+++ client/src/Components/Home/HomeBanner.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function HomeBanner() {
     return (
         <div className = "home__banner">
-            <i class=" home__banner-icon fas fa-2x fa-money-bill-wave"></i>
+            <i className=" home__banner-icon fas fa-2x fa-money-bill-wave"></i>
             <h5>Get a head start on saving with our
             round up program now!  </h5>
         </div>
